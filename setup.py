5ba0adff3fb0e616fe690ce93818cd5441f38c7b
Christopher Adams <christopher.r.adams@gmail.com>
2011-10-27 17:03:56 -0400

    Added simply setup.py file for distutils.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='subuno',
+      version='1.0',
+      py_modules=['subuno'],
+      description='Subuno Python API Library',
+      author='MERS Technologies, LLC',
+      author_email='support@subuno.com',
+      url='http://www.subuno.com/developers.html',
+      )
